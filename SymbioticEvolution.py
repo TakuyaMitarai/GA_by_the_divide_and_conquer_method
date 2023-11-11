28f5d450abd684293304e621ea3508fa433ff708
TakuyaMitarai <miterphoto@gmail.com>
2023-11-11 22:29:14 +0900

    さっきの全然できてなかったから修正

--- SymbioticEvolution.py
+++ SymbioticEvolution.py
@@ -1,37 +1,5 @@
 import numpy as np
 
-# 部分解個体
-class PartialIndividual:
-    def __init__(self, PCHROM_LEN):
-        self.chrom = np.random.randint(0, 2, PCHROM_LEN)
-        self.fitness = 1000000
-
-# 部分解集団
-class PartialPopulation:
-    def __init__(self, PPOP_SIZE, PCHROM_LEN):
-        self.population = []
-        for i in range(PPOP_SIZE):
-            individual = PartialIndividual(PCHROM_LEN)
-            self.population.append(individual)
-
-
-# 全体解個体
-class WholeIndividual:
-    def __init__(self, ppop, WCHROM_LEN, PPOP_SIZE):
-        self.chrom = ppop[np.random.randint(0, PPOP_SIZE, WCHROM_LEN)]
-        self.fitness = 1000000
-
-# 全体解集団
-class WholePopulation:
-    def __init__(self, ppop, WPOP_SIZE, WCHROM_LEN, PPOP_SIZE):
-        self.population = []
-        for i in range(WPOP_SIZE):
-            individual = WholeIndividual(ppop, WCHROM_LEN, PPOP_SIZE)
-            self.population.append(individual)
-
-def evaluate_fitness(wpop, ppop):
-    #なんか書く
-
 # ハイパーパラメータ
 WPOP_SIZE = 200
 PPOP_SIZE = 200
@@ -43,9 +11,43 @@ PMUTATE_PROB = 0.01
 WCHROM_LEN = 8
 PCHROM_LEN = 8
 
+# 部分解個体
+class PartialIndividual:
+    def __init__(self):
+        self.chrom = np.random.randint(0, 2, PCHROM_LEN)
+        self.fitness = 1000000
+
+# 部分解集団
+class PartialPopulation:
+    def __init__(self):
+        self.population = []
+        for i in range(PPOP_SIZE):
+            individual = PartialIndividual()
+            self.population.append(individual)
+
+
+# 全体解個体
+class WholeIndividual:
+    def __init__(self):
+        self.chrom = []
+        for _ in range(WCHROM_LEN):
+            index = np.random.randint(0, PPOP_SIZE)
+            self.chrom.append(ppop.population[index].chrom)
+
+        self.fitness = 1000000
+
+# 全体解集団
+class WholePopulation:
+    def __init__(self):
+        self.population = []
+        for i in range(WPOP_SIZE):
+            individual = WholeIndividual()
+            self.population.append(individual)
+
+def evaluate_fitness():
+    print(wpop.population[0].chrom[0][0])
+
 #初期化
-ppop = PartialPopulation(PPOP_SIZE, PCHROM_LEN)
-wpop = WholePopulation(ppop, WPOP_SIZE, WCHROM_LEN, PPOP_SIZE)
-evaluate_fitness(wpop, ppop)
-
-
+ppop = PartialPopulation()
+wpop = WholePopulation()
+evaluate_fitness()
